9fe0f85adb5a05efc287a5dc002a56af77804972
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-04-10 22:49:12 -0300

    trabajo terminado

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsuario = 0;
+    public Text myText;
+
+    void Start()
+    {
+        if(edadUsuario >= 18)
+        {
+            myText.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            myText.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+}
